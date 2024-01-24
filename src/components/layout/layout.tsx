1a2c0960f5cdfa4a9a8fc5bceeedac6fb00dec13
Esther <elvynnn@gmail.com>
2024-01-24 09:16:11 +0900

    changing order in layout component

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
-import MainNavigation from "./MainNavigation";
 import NavInfo from "./NavInfo";
 import { LangContextProvider } from '@/store/langContext';
+import MainNavigation from "./MainNavigation";
+
 
 
 const Layout = (props: any) => {
@@ -8,9 +9,9 @@ const Layout = (props: any) => {
   return (
       <main>
        <LangContextProvider>
+            {props.children}
             <MainNavigation />
             <NavInfo />  
-            {props.children}
         </LangContextProvider> 
       </main> 
   );
